54227f4638f218b3de7bdbd59b86ba632d5f8c06
Kevin Sawicki <kevinsawicki@gmail.com>
2014-06-19 17:32:15 -0700

    Load functions relative to destination dir
    Closes #3

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,8 +1,10 @@
-. '.\functions.ps1'
+$dest = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+
+. $(Join-Path $dest "functions.ps1")
+
 $packageName = 'Atom' # arbitrary name for the package, used in messages
 $url = 'https://github.com/bradgearon/atom/releases/download/v0.101.0/Atom.zip' # download url
 $url64 = $url # 64bit URL here or just use the same as $url
-$dest = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
 $link = Join-Path $desktop "$packageName.lnk"
 Install-ChocolateyZipPackage "$packageName" "$url" "$dest" "$url64"
