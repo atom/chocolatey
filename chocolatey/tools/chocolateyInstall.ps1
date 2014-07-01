dff2f9fd6d72dfaf53a9a788b860f6141e49f86a
Kevin Sawicki <kevinsawicki@gmail.com>
2014-07-01 11:39:20 -0700

    Group variables with functions

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $dest = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $packageName = 'Atom' # arbitrary name for the package, used in messages
 $url = 'https://github.com/bradgearon/atom/releases/download/v0.106.0/Atom.zip' # download url
 $url64 = $url # 64bit URL here or just use the same as $url
-$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
-$link = Join-Path $desktop "$packageName.lnk"
 Install-ChocolateyZipPackage "$packageName" "$url" "$dest" "$url64"
 
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$link = Join-Path $desktop "$packageName.lnk"
 Install-ChocolateyShortcut -shortcutFilePath $link -targetPath "$dest\Atom.exe"
