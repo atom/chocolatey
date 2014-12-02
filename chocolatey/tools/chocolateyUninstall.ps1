e8497267c45d8bae55b92284e7ee5b26b2b2c445
Kevin Sawicki <kevinsawicki@gmail.com>
2014-12-02 08:53:25 -0800

    Uninstall explorer context menus

--- chocolatey/tools/chocolateyUninstall.ps1
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -13,6 +13,14 @@ $githubStartMenuGroup = Join-Path $startMenu "GitHub, Inc"
 $startMenuLink = Join-Path $githubStartMenuGroup "$packageName.lnk"
 if (Test-Path ($startMenuLink)) {Remove-Item $startMenuLink -force}
 
+# Uninstall explorer menu file, directory, and background shortcuts
+$elevated = @()
+  $elevated += "reg delete HKCR\*\shell\Atom /f"
+  $elevated += "reg delete HKCR\directory\shell\Atom /f"
+  $elevated += "reg delete HKCR\directory\background\shell\Atom /f"
+$cmd = $($elevated -join ';')
+Start-ChocolateyProcessAsAdmin $cmd
+
 # Uninstall apm shortcut
 $apmPath = Join-Path $nugetExePath "apm.bat"
 if (Test-Path ($apmPath)) {Remove-Item $apmPath -force}
