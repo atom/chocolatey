f53298fb7e64a89213a10a0db40eb3aa15858ecc
Kevin Sawicki <kevinsawicki@gmail.com>
2014-12-02 08:50:37 -0800

    Don't create item during uninstall

--- chocolatey/tools/chocolateyUninstall.ps1
+++ chocolatey/tools/chocolateyUninstall.ps1
@@ -10,7 +10,6 @@ if (Test-Path ($desktopLink)) {Remove-Item $desktopLink -force}
 # Uninstall start menu shortcut
 $startMenu = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Programs))
 $githubStartMenuGroup = Join-Path $startMenu "GitHub, Inc"
-New-Item $githubStartMenuGroup -type directory -force
 $startMenuLink = Join-Path $githubStartMenuGroup "$packageName.lnk"
 if (Test-Path ($startMenuLink)) {Remove-Item $startMenuLink -force}
 
