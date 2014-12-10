077116150321fd5ce6b43f95cc49664a8ad1906d
Kevin Sawicki <kevinsawicki@gmail.com>
2014-12-10 14:47:17 -0800

    Remove install args

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,6 +1,5 @@
 $packageName = 'Atom'
 $url = 'https://atom.io/download/windows'
 $installerType = 'exe'
-$installArgs = '/quiet'
 
-Install-ChocolateyPackage $packageName $installerType $installArgs $url
+Install-ChocolateyPackage $packageName $installerType $url
