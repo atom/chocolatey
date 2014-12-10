444ae3af8c4ef23a70b1b6779af83c1c9ca9de88
Kevin Sawicki <kevinsawicki@gmail.com>
2014-12-10 14:49:07 -0800

    Add silent args

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,6 @@
 $packageName = 'Atom'
 $url = 'https://atom.io/download/windows'
 $installerType = 'exe'
+$silentArgs = '--silent'
 
-Install-ChocolateyPackage $packageName $installerType $url
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url
