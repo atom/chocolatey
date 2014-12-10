ec34809f89064f671f2bd0410e4e342f2f6acb8e
Kevin Sawicki <kevinsawicki@gmail.com>
2014-12-10 14:46:06 -0800

    Use vars

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,1 +1,6 @@
-Install-ChocolateyPackage 'Atom' 'exe' '/quiet' 'https://atom.io/download/windows'
+$packageName = 'Atom'
+$url = 'https://atom.io/download/windows'
+$installerType = 'exe'
+$installArgs = '/quiet'
+
+Install-ChocolateyPackage $packageName $installerType $installArgs $url
