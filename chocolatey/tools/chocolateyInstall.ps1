86f4edf3f1421e5f7b12ac969dc6e4a03dd091ba
Kevin Sawicki <kevinsawicki@gmail.com>
2014-12-11 11:58:21 -0800

    Remove old apm shim on upgrade

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -1,3 +1,8 @@
+# Remove old apm shim
+$apmPath = Join-Path $nugetExePath "apm.bat"
+if (Test-Path ($apmPath)) {Remove-Item $apmPath -force}
+
+# Run installer
 $packageName = 'Atom'
 $url = 'https://atom.io/download/windows'
 $installerType = 'exe'
