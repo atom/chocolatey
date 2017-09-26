d9c4b50ff04ba5f400f760bb7118e67377a6fc0b
Saran Tanpituckpong <sarunta@gmail.com>
2017-09-26 17:32:52 +0700

    Add 64 bit support

--- chocolatey/tools/chocolateyInstall.ps1
+++ chocolatey/tools/chocolateyInstall.ps1
@@ -9,7 +9,8 @@ if (Test-Path ($atomPath)) {Remove-Item $atomPath -force}
 # Run installer
 $packageName = 'Atom'
 $url = 'https://atom.io/download/windows'
+$url64 = 'https://atom.io/download/windows_x64'
 $installerType = 'exe'
 $silentArgs = '--silent'
 
-Install-ChocolateyPackage $packageName $installerType $silentArgs $url
+Install-ChocolateyPackage $packageName $installerType $silentArgs $url $url64
